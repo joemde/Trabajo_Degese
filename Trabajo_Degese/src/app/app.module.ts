f5e27f96fd86efa964112378e19b463f0913999c
joemde <111288020+joemde@users.noreply.github.com>
2022-08-24 14:05:20 +0200

    se le agregaron parte de los componentes

--- Trabajo_Degese/src/app/app.module.ts
+++ Trabajo_Degese/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
