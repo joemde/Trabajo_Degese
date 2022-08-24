1b44908ad5397a695e7b964fd027e912631c31a6
joemde <111288020+joemde@users.noreply.github.com>
2022-08-24 14:28:39 +0200

    se le agregaron mas componentes

--- Trabajo_Degese/src/app/app.module.ts
+++ Trabajo_Degese/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { SocialComponent } from './components/social/social.component';
     HeaderComponent,
     BannerComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
